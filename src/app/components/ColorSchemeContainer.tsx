f37df714c12a3e961c314a9d1550c9466d5f982e
Michelle Stermitz <stermitzm@gmail.com>
2023-09-02 22:12:19 -0700

    Display color scheme with new styled component

--- src/app/components/ColorSchemeContainer.tsx
+++ src/app/components/ColorSchemeContainer.tsx
@@ -1,5 +1,16 @@
 "use client"
 import React from 'react'
+import { styled } from 'styled-components'
+
+const Button = styled.button<{ $hexColor: any; }>`
+  border-radius: 3px;
+  display: inline-block;
+  margin: 0.5rem 1rem;
+  padding: 0.5rem 0;
+  transition: all 200ms ease-in-out;
+  width: 200px;
+  background-color: ${props => props.$hexColor};
+`
 
 const getData = async () => {
   const res = await fetch('https://www.thecolorapi.com/scheme?hex=24B1E0')
@@ -15,10 +26,13 @@ const getData = async () => {
 export const ColorSchemeContainer: React.FC = async () => {
   const data = await getData()
   console.log("DATA IN HOME", data.colors)
+
   return (
     <>{data.colors.map((color: any) => {
       return (
-        <>{JSON.stringify(color)}</>
+        <>
+        <Button $hexColor={color.hex.value}>asdfasdf</Button>
+        </>
       )
     })}</>
   )
