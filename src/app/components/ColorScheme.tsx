c77c40935811f69a2ee7cd562adc43776b505c55
Michelle Stermitz <stermitzm@gmail.com>
2023-09-12 17:59:24 -0600

    Linear gradient background

--- src/app/components/ColorScheme.tsx
+++ src/app/components/ColorScheme.tsx
@@ -1,32 +1,45 @@
-"use client"
-import React from 'react'
-import { styled } from 'styled-components'
+"use client";
+import React from "react";
+import { styled } from "styled-components";
 
 interface ColorSchemeProps {
-  readonly colors?: any
+  readonly colors?: any;
 }
 
-const ColorButton = styled.button<{ $hexColor: any; }>`
+const ColorButton = styled.button<{ $rgbColor: any }>`
   border-radius: 15px;
   display: inline-block;
   margin: 0.5rem 1rem;
-  padding: 0.5rem 0;
+  padding: 1rem 0;
   transition: all 200ms ease-in-out;
-  width: 11rem;
+  width: 12rem;
   color: white;
-  background-color: ${props => props.$hexColor};
-`
+  background: linear-gradient(
+    90deg,
+    ${(props) => props.$rgbColor} 0%,
+    ${(props) => props.$rgbColor} 35%,
+    ${(props) => props.$rgbColor} 100%
+  );
+`;
 
 export const ColorScheme: React.FC<ColorSchemeProps> = ({ colors }) => {
-  console.log("Colors in color scheme", colors)
+  console.log("Colors in color scheme", colors);
   return (
     <>
-    {colors ? colors.map((color: any, i: number) => {
-      return (
-        <div key={i}>
-        <ColorButton $hexColor={color.hex.value} >{color.name.value}</ColorButton>
-        </div>
-      )
-    }) : <>Loading</>}</>
-  )
-}
+      {colors ? (
+        colors.map((color: any, i: number) => {
+          return (
+            <div key={i}>
+              <ColorButton $rgbColor={color.rgb.value}>
+                {color.name.value}
+              </ColorButton>
+            </div>
+          );
+        })
+      ) : (
+        <>Loading</>
+      )}
+    </>
+  );
+};
+
