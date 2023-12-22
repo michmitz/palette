a9426a865b3d4b9c68f2be4ca9885b239f79c428
Michelle Stermitz <stermitzm@gmail.com>
2023-12-21 17:23:10 -0700

    remove background colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { ColorSchemeContainer } from "./components/ColorSchemeContainer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between bg-red-200 p-20">
-      <div className="bg-orange-50 rounded-2xl p-12 shadow-2xl">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-slate-50 p-20">
+      <div className="p-12 shadow-2xl">
         <ColorSchemeContainer />
       </div>
     </main>
